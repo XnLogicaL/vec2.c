66a7ab1570041dc4271a841718837240afc9f0df
XnLogicaL <140826629+XnLogicaL@users.noreply.github.com>
2025-07-27 22:23:56 +0300

    Fix header

--- vec2.h
+++ vec2.h
@@ -4,9 +4,6 @@
 #include <complex.h>
 #include <math.h>
 
-#define _VX vec2_x(v)
-#define _VY vec2_y(v)
-
 // type
 #define vec2(T) T complex
 
@@ -16,27 +13,23 @@
 #define vec2_y(v) (cimag(v))
 
 // geometric
-#define vec2_magnitude(v) (sqrt(_VX * _VX + _VY * _VY))
+#define vec2_magnitude(v) (sqrt(vec2_x(v) * vec2_x(v) + vec2_y(v) * vec2_y(v)))
 #define vec2_unit(v) (v / vec2_magnitude(v))
-#define vec2_angle(v) (atan2(_VY, _VX))
+#define vec2_angle(v) (atan2(vec2_y(v), vec2_x(v)))
 
 // stuff
 #define vec2_dot(v1, v2) vec2_x((v1)*conj(v2))
 #define vec2_cross(v1, v2) (vec2_y((v1)*conj(v2)))
-#define vec2_proj(v, onto)                                                     \
-  ((vec2(typeof(v)))((vec2_dot(v, onto) / vec2_dot(onto, onto)) * (onto)))
+#define vec2_proj(v, onto) ((vec2_dot(v, onto) / vec2_dot(onto, onto)) * (onto))
 #define vec2_reflect(v, normal) ((v)-2 * vec2_dot(v, normal) * (normal))
 #define vec2_lerp(v1, v2, t) ((1 - (t)) * (v1) + (t) * (v2))
 
 // constants
-#define vec2_zero(T) vec2_new(T(0), T(0))
-#define vec2_one(T) vec2_new(T(1), T(1))
-#define vec2_up(T) vec2_new(T(0), T(1))
-#define vec2_down(T) vec2_new(T(0), T(-1))
-#define vec2_right(T) vec2_new(T(1), T(0))
-#define vec2_left(T) vec2_new(T(-1), T(0))
-
-#undef _VX
-#undef _VY
+#define vec2_zero(T) vec2_new((T)0, (T)0)
+#define vec2_one(T) vec2_new((T)1, (T)1)
+#define vec2_up(T) vec2_new((T)0, (T)1)
+#define vec2_down(T) vec2_new((T)0, (T)(-1))
+#define vec2_right(T) vec2_new((T)1, (T)0)
+#define vec2_left(T) vec2_new((T)(-1), (T)0)
 
 #endif
